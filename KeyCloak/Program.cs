c9f60fd6b727c515cbc090dc2c71ef94e284f4bc
Mohd Imran <mohdimran043@gmail.com>
2019-08-01 21:03:00 +0300

    Add project files.

---
+++ KeyCloak/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace KeyCloak
+{
+    
+//{
+// 'client_id': 'demo-app',
+//    'username': 'imu1',
+//    'password': 'admin',
+//'grant_type': 'password'
+//,'client_secret':''
+
+//}
+
+
+//http://localhost:50883/api/values
+//http://localhost:8080/auth/realms/master/protocol/openid-connect/token
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
